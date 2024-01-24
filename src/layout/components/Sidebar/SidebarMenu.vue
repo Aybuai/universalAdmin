1b17b36610d3be775da2b09d5d7de6d917bfa306
Aybuai <13624448268@163.com>
2024-01-24 12:01:55 +0800

    feat: 临时菜单

---
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-menu
+    :unique-opened="true"
+    default-active="2"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+  >
+    <el-submenu index="1">
+      <template #title>
+        <i class="el-icon-location"></i>
+        <span>导航一</span>
+      </template>
+      <el-menu-item-group title="Group One">
+        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-2">选项2</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="Group Two">
+        <el-menu-item index="1-3">选项3</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+    <el-menu-item index="2">
+      <template #title>
+        <i class="el-icon-setting"></i>
+        <span>导航二</span>
+      </template>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script setup></script>
+<style scoped lang="scss"></style>
