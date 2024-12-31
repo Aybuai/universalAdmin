6eba14688e8e9bfe98e044d74d78d2823d70e48f
Aybuai <13624448268@163.com>
2024-12-31 10:36:04 +0800

    fix: 修改逻辑

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -13,7 +13,7 @@
       @change="onSelectChange"
     >
       <el-option
-        v-for="option in 6"
+        v-for="option in 7"
         :key="option"
         :label="option"
         :value="option"
