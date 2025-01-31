c36c4c7fda101ca1ff69ac8f0df5b8ac2a642209
Aybuai <13624448268@163.com>
2025-01-31 08:22:13 +0800

    fix: 修改格式

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -13,7 +13,7 @@
       @change="onSelectChange"
     >
       <el-option
-        v-for="option in 3"
+        v-for="option in 5"
         :key="option"
         :label="option"
         :value="option"
