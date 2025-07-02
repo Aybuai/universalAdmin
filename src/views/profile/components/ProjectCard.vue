6f249c836c20b231a16bb4305694a8422fe63d08
Aybuai <13624448268@163.com>
2025-07-02 22:01:51 +0800

    feat: 用户头像展示组件 PanThumb

--- src/views/profile/components/ProjectCard.vue
+++ src/views/profile/components/ProjectCard.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>项目介绍</div>
+  <div>
+    <pan-thumb
+      :image="$store.getters.userInfo.avatar"
+      width="100px"
+      height="100px"
+    >
+      <div>Hello</div>
+      {{ $store.getters.userInfo.title }}
+    </pan-thumb>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import PanThumb from '@/components/PanThumb'
+</script>
 <style scoped lang="scss"></style>
